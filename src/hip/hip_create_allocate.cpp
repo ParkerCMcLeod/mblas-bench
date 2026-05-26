6fe788111c296863fd082ae2df814337c43909b6
Evan Masters <emasters@amd.com>
2026-05-26 22:45:44 +0000

    Remove packing count from get_malloc_size_host in HIP

--- src/hip/hip_create_allocate.cpp
+++ src/hip/hip_create_allocate.cpp
@@ -51,10 +51,9 @@ using std::string;
 long long get_malloc_size_host(mblas_data_type type, long x, long y, int batch,
                                long long stride) {
   int typesize = type_call_host<sizeofCUDT>(type);
-  long packing_count = type.get_packing_count();
   long long base = x * y;
   long long total_elements = stride * (batch - 1) + base;
-  return total_elements * typesize * packing_count;
+  return total_elements * typesize;
 }
 
 long long get_malloc_size_dev(mblas_data_type type, long x, long y, int batch,
